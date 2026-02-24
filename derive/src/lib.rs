e4b55ca762bb84c575143da6d93e774867a70bad
Alex Acebo <aacebowork@gmail.com>
2026-02-24 10:33:09 -0800

    stuff

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,1 +1,8 @@
+use proc_macro::TokenStream;
+use quote::quote;
 
+#[proc_macro_derive(Moxy)]
+pub fn derive(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    quote! {}.into()
+}
