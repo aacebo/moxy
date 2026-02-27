2d4d61f5a516d8ed4cc0bd523c16f46eff468e34
aacebo <aacebowork@gmail.com>
2026-02-26 22:53:55 -0500

    Update structs.rs

--- crates/derive/src/build/structs.rs
+++ crates/derive/src/build/structs.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 
-use crate::{Error, Render, core::Field, params};
+use crate::{Render, core::Field, params};
 
 #[derive(Clone, Default)]
 pub struct StructSyntax;
@@ -11,6 +11,8 @@ impl Render for StructSyntax {
 
     fn render(&self, args: Self::Args) -> syn::Result<TokenStream> {
         let ident = &args.input.ident;
+        let build_ident = format_ident!("{}Builder", &ident);
+        let vis = &args.input.vis;
         let (impl_generics, type_generics, where_generics) = &args.input.generics.split_for_impl();
         let fields: Vec<_> = args
             .data
@@ -18,29 +20,56 @@ impl Render for StructSyntax {
             .iter()
             .enumerate()
             .filter_map(|(i, field)| Field::parse(i, field).ok())
+            .filter(|field| field.attrs().exists("build"))
             .collect();
 
-        let field = fields
+        let build_fields: Vec<_> = fields
             .iter()
-            .find(|field| field.attrs().exists("deref"))
-            .or_else(|| fields.first());
+            .map(|field| {
+                let name = field.name();
+                let ty = field.ty();
+                quote!(#name: Option<#ty>)
+            })
+            .collect();
 
-        match field {
-            None => Err(args.input.error("field not found")),
-            Some(field) => {
-                let field_name = field.name();
-                let field_ty = field.ty();
+        let build_methods: Vec<_> = fields
+            .iter()
+            .map(|field| {
+                let name = field.name();
+                let ty = field.ty();
 
-                Ok(quote! {
-                    impl #impl_generics ::std::ops::Deref for #ident #type_generics #where_generics {
-                        type Target = #field_ty;
-
-                        fn deref(&self) -> &Self::Target {
-                            &self.#field_name
-                        }
+                quote! {
+                    pub fn #name(mut self, value: #ty) -> Self {
+                        self.#name = Some(value);
+                        self
                     }
-                })
+                }
+            })
+            .collect();
+
+        let build_fields_assign: Vec<_> = fields
+            .iter()
+            .map(|field| {
+                let name = field.name();
+                quote!(#name: self.#name.expect("required"))
+            })
+            .collect();
+
+        Ok(quote! {
+            #[derive(Default)]
+            #vis struct #build_ident #type_generics #where_generics {
+                #(#build_fields,)*
             }
-        }
+
+            impl #impl_generics #build_ident #type_generics #where_generics {
+                #(#build_methods)*
+
+                pub fn build(self) -> #ident {
+                    #ident {
+                        #(#build_fields_assign,)*
+                    }
+                }
+            }
+        })
     }
 }
