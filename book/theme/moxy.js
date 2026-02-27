4194c6e1dd805a92823ae0cd24ab7514ae0e9cb7
Alex Acebo <aacebowork@gmail.com>
2026-02-27 12:46:48 -0800

    update docs

--- book/theme/moxy.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var toggle = document.getElementById('theme-toggle');
-    var list = document.getElementById('theme-list');
-    if (toggle) toggle.remove();
-    if (list) list.remove();
-})();
